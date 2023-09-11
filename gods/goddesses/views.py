191e70320677cb214ebf4b1187f7c42cbd19d3f1
Tais <72388962+TeaThais@users.noreply.github.com>
2023-09-11 18:34:59 +0200

    user converters

--- gods/goddesses/views.py
+++ gods/goddesses/views.py
@@ -6,5 +6,13 @@ def index(request):
     return HttpResponse('The very first page')
 
 
-def categories(request):
-    return HttpResponse('Categories')
+def categories(request, cat_id):
+    return HttpResponse(f"Categories <p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"Categories <p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"Archive for {year} year")
