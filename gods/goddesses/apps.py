70b6f730615bcbd49807f37caf2751a6e7861e92
Tais <72388962+TeaThais@users.noreply.github.com>
2023-09-11 12:16:39 +0200

    Initial commit

---
+++ gods/goddesses/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GoddessesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'goddesses'
---
+++ gods/goddesses/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('The very first page')
+
+
+def categories(request):
+    return HttpResponse('Categories')
